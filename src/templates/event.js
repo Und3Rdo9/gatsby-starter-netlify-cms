3d370996f0b75686fcbb11d83991fc1ea324a710
Tim Bakkum <timbakkum@gmail.com>
2018-06-14 15:19:09 +0200

    adds an event content type, moves all markdown files to /content folder

--- src/templates/event.js
+++ src/templates/event.js
@@ -5,11 +5,10 @@ import Helmet from 'react-helmet';
 import Link from 'gatsby-link';
 import Content, { HTMLContent } from '../components/Content';
 
-export const BlogPostTemplate = ({
+export const EventTemplate = ({
   content,
   contentComponent,
   description,
-  tags,
   title,
   helmet
 }) => {
@@ -26,18 +25,6 @@ export const BlogPostTemplate = ({
             </h1>
             <p>{description}</p>
             <PostContent content={content} />
-            {tags && tags.length ? (
-              <div style={{ marginTop: `4rem` }}>
-                <h4>Tags</h4>
-                <ul className="taglist">
-                  {tags.map(tag => (
-                    <li key={tag + `tag`}>
-                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ) : null}
           </div>
         </div>
       </div>
@@ -45,7 +32,7 @@ export const BlogPostTemplate = ({
   );
 };
 
-BlogPostTemplate.propTypes = {
+EventTemplate.propTypes = {
   content: PropTypes.string.isRequired,
   contentComponent: PropTypes.func,
   description: PropTypes.string,
@@ -53,11 +40,11 @@ BlogPostTemplate.propTypes = {
   helmet: PropTypes.instanceOf(Helmet)
 };
 
-const BlogPost = ({ data }) => {
+const Event = ({ data }) => {
   const { markdownRemark: post } = data;
 
   return (
-    <BlogPostTemplate
+    <EventTemplate
       content={post.html}
       contentComponent={HTMLContent}
       description={post.frontmatter.description}
@@ -68,16 +55,16 @@ const BlogPost = ({ data }) => {
   );
 };
 
-BlogPost.propTypes = {
+Event.propTypes = {
   data: PropTypes.shape({
     markdownRemark: PropTypes.object
   })
 };
 
-export default BlogPost;
+export default Event;
 
 export const pageQuery = graphql`
-  query BlogPostByID($id: String!) {
+  query EventByID($id: String!) {
     markdownRemark(id: { eq: $id }) {
       id
       html
@@ -85,7 +72,6 @@ export const pageQuery = graphql`
         date(formatString: "MMMM DD, YYYY")
         title
         description
-        tags
       }
     }
   }
