ae0d53f227ccd298cf42035dcb31627427bd137f
Tim Bakkum <timbakkum@gmail.com>
2018-06-25 22:08:25 +0200

    adapts team tpl

---
+++ src/templates/team.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { kebabCase } from 'lodash';
+import Helmet from 'react-helmet';
+import Link from 'gatsby-link';
+import Content, { HTMLContent } from '../components/Content';
+
+export const TeamTemplate = ({
+  content,
+  contentComponent,
+  description,
+  title,
+  helmet
+}) => {
+  const PostContent = contentComponent || Content;
+
+  return (
+    <section className="section">
+      {helmet || ''}
+      <div className="container content">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
+              {title}
+            </h1>
+            <p>{description}</p>
+            <PostContent content={content} />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+TeamTemplate.propTypes = {
+  content: PropTypes.string.isRequired,
+  contentComponent: PropTypes.func,
+  description: PropTypes.string,
+  title: PropTypes.string,
+  helmet: PropTypes.instanceOf(Helmet)
+};
+
+const Team = ({ data }) => {
+  const { markdownRemark: post } = data;
+
+  return (
+    <TeamTemplate
+      content={post.html}
+      contentComponent={HTMLContent}
+      description={post.frontmatter.description}
+      helmet={<Helmet title={`${post.frontmatter.title} | Blog`} />}
+      tags={post.frontmatter.tags}
+      title={post.frontmatter.title}
+    />
+  );
+};
+
+Team.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object
+  })
+};
+
+export default Team;
+
+export const teamQuery = graphql`
+  query TeamByID($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
+      html
+      frontmatter {
+        title
+        description
+      }
+    }
+  }
+`;
